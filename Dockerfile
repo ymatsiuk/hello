490a65171d1ee1a72f9f85dbef7c5c0c2b21b4dc
Yurii Matsiuk <24990891+ymatsiuk@users.noreply.github.com>
2023-05-15 11:09:44 +0200

    Add prometheus metrics using middleware

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,8 @@
-FROM golang:latest as builder
-RUN go get -d -v github.com/ymatsiuk/hello 
-WORKDIR /go/src/github.com/ymatsiuk/hello
-RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o hello .
-
-FROM gruebel/upx:latest as upx
-COPY --from=builder /go/src/github.com/ymatsiuk/hello/hello /hello.orig
-RUN upx --best --lzma -o /hello /hello.orig
-
+FROM golang:alpine AS builder
+WORKDIR $GOPATH/src/ymatsiuk/hello
+COPY . .
+RUN CGO_ENABLED=0 GOOS=linux go build -ldflags="-w -s" -o /go/bin/hello
 FROM scratch
-COPY --from=upx /hello /hello
-CMD ["/hello"] 
-
+COPY --from=builder /go/bin/hello /go/bin/hello
+COPY --from=alpine:latest /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/
+ENTRYPOINT ["/go/bin/hello"]
--- main.go
+++ main.go
@@ -1,7 +1,77 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	"github.com/jessicalins/instrumentation-practices-examples/middleware/httpmiddleware"
+)
 
 func main() {
-	fmt.Println("hello world")
+	// Create a new random number generator
+	rand.Seed(time.Now().UnixNano())
+
+	// Create non-global registry.
+	registry := prometheus.NewRegistry()
+
+	// Add go runtime metrics and process collectors.
+	registry.MustRegister(
+		collectors.NewGoCollector(),
+		collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}),
+	)
+
+	// Expose /metrics HTTP endpoint using the created custom registry.
+	http.Handle(
+		"/metrics",
+		httpmiddleware.New(
+			registry, nil).
+			WrapHandler("/metrics", promhttp.HandlerFor(
+				registry,
+				promhttp.HandlerOpts{}),
+			))
+
+    // Expose /healthz endpoint used by k8s probes
+	http.Handle(
+		"/healthz",
+		httpmiddleware.New(
+			registry, nil).
+			WrapHandler("/healthz",
+				func() http.Handler {
+					return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+						// Simulate DB ping for example up to 300ms
+						time.Sleep(time.Millisecond * time.Duration(rand.Intn(300)))
+						w.WriteHeader(http.StatusOK)
+						w.Write([]byte("OK"))
+					})
+				}()))
+
+    // Expose main catch-all endpoint that fails randomly with 500
+	http.Handle(
+		"/",
+		httpmiddleware.New(
+			registry, nil).
+			WrapHandler("/",
+				func() http.Handler {
+					return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+						// Simulate some heavier work up to 1s
+						time.Sleep(time.Millisecond * time.Duration(rand.Intn(1000)))
+
+						// Fail randomly to make it interesting
+						if rand.Intn(30) == 3 {
+							w.WriteHeader(http.StatusInternalServerError)
+							return
+						}
+
+						w.WriteHeader(http.StatusOK)
+						w.Write([]byte("Hello, world!"))
+					})
+				}()))
+
+	log.Fatalln(http.ListenAndServe(":8080", nil))
 }
